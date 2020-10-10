50c4c3818b810614fc431ab6aeae44d3f9f14bdf
Mustodonti <vankontakt@mail.ru>
2020-10-10 22:50:36 +0300

    f

--- WeatherService/CityTimestampMiddleware.cs
+++ WeatherService/CityTimestampMiddleware.cs
@@ -12,10 +12,12 @@ namespace WeatherService
     public class CityTimestampMiddleware
     {
         private readonly RequestDelegate _next;//ссылка на тот делегат запроса, который стоит следующим в конвейере обработки запроса.
+        private  string _openWeatherAPIkey;
 
         public CityTimestampMiddleware(RequestDelegate next)
         {
             _next = next;
+            _openWeatherAPIkey =System.Environment.GetEnvironmentVariable("OWM_API_KEY"); ;
         }
 
         public async Task InvokeAsync(HttpContext context)
@@ -28,7 +30,7 @@ namespace WeatherService
                 var timestamp = Convert.ToInt32(context.Request.Query["timestamp"]);
                 if ((!string.IsNullOrWhiteSpace(city) & timestamp > 0 & timestamp < 97))
                 {
-                    var keyapi = new KeyAPI("2d6d053b6a777cd0fe236dd2c0d9aa22");
+                    var keyapi = new KeyAPI(_openWeatherAPIkey);
                     var result = keyapi.DetectedForecastWeather(city, timestamp.ToString());
 
                     await context.Response.WriteAsync(result);
@@ -40,7 +42,7 @@ namespace WeatherService
                 var city = context.Request.Query["city"];
                 if (!string.IsNullOrWhiteSpace(city))
                 {
-                    var keyapi = new KeyAPI("2d6d053b6a777cd0fe236dd2c0d9aa22");
+                    var keyapi = new KeyAPI(_openWeatherAPIkey);
                     var result = keyapi.DetectedCurrentWeather(city);
 
                     await context.Response.WriteAsync(result);
